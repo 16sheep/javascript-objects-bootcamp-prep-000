7de5b838dd0154b1be0f01cbfa4b21da3e5435c6
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-29 16:39:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,10 +8,8 @@ let playlist = {deftones:'Minerva'};
 
 
 function updatePlaylist (obj, key, value) {
-   let obj2 = Object.assign({},obj, { [key]: value });
-   console.log(obj)
-  console.log(obj2)
-   return obj2
+  obj[key] = value
+  return obj
   
 }
 
